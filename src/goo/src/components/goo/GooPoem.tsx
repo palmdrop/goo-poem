7ac37e4cbedcc94c98e0899c7c1f4dad31df21e5
palmdrop <anton@exlex.se>
2024-02-22 10:11:41 +0100

    refactor(goo): cleanup comments and unused props

--- src/goo/src/components/goo/GooPoem.tsx
+++ src/goo/src/components/goo/GooPoem.tsx
@@ -1,24 +1,13 @@
-import { createSignal, type Component, createMemo } from 'solid-js';
+import { createSignal, type Component } from 'solid-js';
 import { GooPoem } from '../../types/goo-poem';
 import { flowLoop } from '../../core/flow';
 
-const gooPoem: Component<GooPoem> = ({ log, value })=> {
+const gooPoem: Component<GooPoem> = ({ log })=> {
   const [line, setLine] = createSignal('');
 
   flowLoop(log, action => {
     setLine(action.value);
   });
-  /*
-  const [index, setIndex] = createSignal(0);
-
-  const line = createMemo(() => {
-    return log[index()].value;
-  });
-
-  setInterval(() => {
-    setIndex((index() + 1) % log.length);
-  }, 500)
-  */
 
   return (
     <main>
--- src/goo/src/constants.tsx
+++ src/goo/src/constants.tsx
@@ -1,3 +1,5 @@
 export const MIN_CHANGE_DELAY = 10;
 export const MAX_CHANGE_DELAY = 2000;
 export const LOOP_ITERATION_DELAY = 2000;
+
+export const MAX_ANIMATION_TIME = 500;
