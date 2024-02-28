0c83204adb0e05c96a6de333e6568183e2f12249
palmdrop <anton@exlex.se>
2024-02-28 17:52:59 +0100

    style(goo): better positioning and animation for timestep markers

--- src/goo/src/components/progress/Progress.tsx
+++ src/goo/src/components/progress/Progress.tsx
@@ -4,6 +4,8 @@ import { ChangeLog } from "../../types/poem";
 import styles from './Progress.module.css';
 import { getDelay } from "../../core/flow";
 
+const padding = 0.01;
+
 export const Progress: Component<{ index: number, delay: number, log: ChangeLog }> = props => {
   const durations = createMemo(() => {
     let accumulator = 0;
@@ -18,7 +20,7 @@ export const Progress: Component<{ index: number, delay: number, log: ChangeLog 
   const completeDuration = createMemo(() => durations().at(-1)!);
 
   const getProgress = (index: number) => {
-    return durations()[index] / completeDuration();
+    return padding + (durations()[index] / completeDuration()) * (1 - 2 * padding);
   }
 
   const progress = createMemo(
@@ -35,21 +37,25 @@ export const Progress: Component<{ index: number, delay: number, log: ChangeLog 
     >
       <div class={styles.progress}>
         <div class={styles.bar} />
-        <Index
-          each={props.log}
-        >{(_, i) => {
-          const progress = getProgress(i);
-          return (
-            <button 
-              class={`${styles.indicator} ${i < props.index ? styles.passed : ''}`}
-              style={`
-                --progress: ${progress};
-              `}
-            >
-            </button>
-          );
-        }}</Index>
       </div>
+      <Index
+        each={props.log}
+      >{(_, i) => {
+        const progress = getProgress(i);
+        return (
+          <div
+            class={`${styles.indicator} ${i <= props.index ? styles.passed : ''}`}
+            style={`
+              --progress: ${progress};
+            `}
+          >
+            <button 
+              class={styles.button}
+            />
+            <div class={styles.line}></div>
+          </div>
+        );
+      }}</Index>
     </div>
   )
 }
