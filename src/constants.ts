9deecc53e0b12f5aa4cdd46a0bde314e7e1e0ec9
palmdrop <anton@exlex.se>
2024-05-29 13:40:31 +0200

    fix: increase delay

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,8 @@ export const LOG_FILE = './src/log.json';
 export const DATE_END_INDEX = 24;
 export const POEM_START_INDEX = DATE_END_INDEX + 2;
 
-export const MIN_CHANGE_DELAY = 1500;
-export const MAX_CHANGE_DELAY = 3500;
+export const MIN_CHANGE_DELAY = 2000;
+export const MAX_CHANGE_DELAY = 4000;
 
 export const DELAY_CONTRAST_THRESHOLD = 0.9;
 export const SHORT_DELAY_CONTRAST = 3;
@@ -14,6 +14,6 @@ export const LONG_DELAY_CONTRAST = 2;
 export const ORB_MIN_SIZE_MULTIPLIER = 0.3;
 export const ORB_MAX_SIZE_MULTIPLIER = 1.0;
 
-export const LOOP_ITERATION_DELAY = MAX_CHANGE_DELAY;
+export const LOOP_ITERATION_DELAY = MAX_CHANGE_DELAY * 1.5;
 
 export const MAX_ANIMATION_TIME = 1000;
